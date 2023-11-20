1a88ff5fae5ede987e7babbc2bc118b98e7a8a18
maksonkapysta <144914166+maksonkapysta@users.noreply.github.com>
2023-11-21 00:18:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var button_random = document.getElementById('random');
+var text = document.getElementById('phrase');
+
+function getRandomPhrase() {
+  const phrases = [
+    'бля я пидор',
+    'шесть шесть шесть мои руки прада блейдс',
+    'вот бы в речке поплавать так туда сюда крабов поймать и съесть)))0))',
+    'пи пи пи пидор гей пи пи пидор гей, он пидорас',
+    'я делаю урлки и смотрю соньку зодотую.оучку'
+  ];
+
+  const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+  document.getElementById('phrase').textContent = randomPhrase;
+}
